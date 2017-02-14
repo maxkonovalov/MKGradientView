8b78af66b4c916cc3416a4abb0b093919694e911
Max Konovalov <konovalovmy@gmail.com>
2017-02-14 15:07:41 +0300

    Comments cleanup

--- MKGradientView/MKGradientView.swift
+++ MKGradientView/MKGradientView.swift
@@ -156,86 +156,76 @@ extension MKGradientView {
 
 open class MKGradientLayer: CALayer {
     
-    /* The array of CGColorRef objects defining the color of each gradient stop.
-    * For `.bilinear` gradient type defines X-direction gradient stops. */
-    
+    /// The array of CGColorRef objects defining the color of each gradient stop.
+    /// For `.bilinear` gradient type defines X-direction gradient stops.
     open var colors: [CGColor] = [UIColor.clear.cgColor, UIColor.clear.cgColor] {
         didSet {
             setNeedsDisplay()
         }
     }
     
-    /* The array of Y-direction gradient stops for `.bilinear` gradient type.
-    * Ignored for other gradient types. */
-    
+    /// The array of Y-direction gradient stops for `.bilinear` gradient type.
+    /// Ignored for other gradient types.
     open var colors2: [CGColor] = [UIColor.clear.cgColor, UIColor.clear.cgColor] {
         didSet {
             setNeedsDisplay()
         }
     }
     
-    /* An optional array of Floats defining the location of each
-    * gradient stop as a value in the range [0.0, 1.0]. The values must be
-    * monotonically increasing. If a nil array is given, the stops are
-    * assumed to spread uniformly across the [0.0, 1.0] range. The number
-    * of elements must be equal to `colors` array count.
-    * Defines X-direction color locations for `.bilinear` gradient type. */
-    
+    /// An optional array of Floats defining the location of each gradient stop as a value in the range [0.0, 1.0]. The values must be monotonically increasing. If a nil array is given, the stops are assumed to spread uniformly across the [0.0, 1.0] range. The number of elements must be equal to `colors` array count.
+    /// Defines X-direction color locations for `.bilinear` gradient type.
     open var locations: [Float]? {
         didSet {
             setNeedsDisplay()
         }
     }
     
-    /* An optional array of Y-direction color locations for `.bilinear`
-    * gradient type. The number of elements must be equal to `colors2`
-    * array count. Ignored for other gradient types. */
-    
+    /// An optional array of Y-direction color locations for `.bilinear` gradient type. The number of elements must be equal to `colors2` array count. 
+    /// Ignored for other gradient types.
     open var locations2: [Float]? {
         didSet {
             setNeedsDisplay()
         }
     }
     
-    /* The start and end points of the gradient when drawn into the layer's
-    * coordinate space. The start point corresponds to the first gradient
-    * stop, the end point to the last gradient stop. Both points are
-    * defined in a unit coordinate space that is then mapped to the
-    * layer's bounds rectangle when drawn. (I.e. [0.0, 0.0] is the bottom-left
-    * corner of the layer, [1.0, 1.0] is the top-right corner.)
-    * The default values for gradient types are:
-    * `.linear`:  [0.5, 0.0] -> [0.5, 1.0]
-    * `.radial`:  [0.5, 0.5] -> [1.0, 0.5]
-    * `.conical`: [0.5, 0.5] -> [1.0, 0.5]
-    * `.bilinear` (X-direction): [0.0, 0.5] -> [1.0, 0.5] */
-    
+    /// The start point of the gradient when drawn into the layer's coordinate space. The start point corresponds to the first gradient stop. The points are defined in a unit coordinate space that is then mapped to the layer's bounds rectangle when drawn. (I.e. [0.0, 0.0] is the bottom-left corner of the layer, [1.0, 1.0] is the top-right corner.).
+    ///
+    /// The default values for gradient types are:
+    ///
+    /// - `.linear`:  [0.5, 0.0] -> [0.5, 1.0]
+    /// - `.radial`:  [0.5, 0.5] -> [1.0, 0.5]
+    /// - `.conical`: [0.5, 0.5] -> [1.0, 0.5]
+    /// - `.bilinear` (X-direction): [0.0, 0.5] -> [1.0, 0.5]
     open var startPoint: CGPoint? {
         didSet {
             setNeedsDisplay()
         }
     }
+    
+    /// The end point of the gradient when drawn into the layer's coordinate space. The end point corresponds to the last gradient stop. The points are defined in a unit coordinate space that is then mapped to the layer's bounds rectangle when drawn. (I.e. [0.0, 0.0] is the bottom-left corner of the layer, [1.0, 1.0] is the top-right corner.).
     open var endPoint: CGPoint? {
         didSet {
             setNeedsDisplay()
         }
     }
     
-    /* The start and end points of the `.bilinear` gradient's Y-direction,
-    * defaults to [0.5, 0.0] -> [0.5, 1.0]. Ignored for other gradient types. */
-    
+    /// The start point of the `.bilinear` gradient's Y-direction, defaults to [0.5, 0.0] -> [0.5, 1.0].
+    /// Ignored for other gradient types.
     open var startPoint2: CGPoint? {
         didSet {
             setNeedsDisplay()
         }
     }
+    
+    /// The end point of the `.bilinear` gradient's Y-direction, defaults to [0.5, 0.0] -> [0.5, 1.0].
+    /// Ignored for other gradient types.
     open var endPoint2: CGPoint? {
         didSet {
             setNeedsDisplay()
         }
     }
     
-    /* The kind of gradient that will be drawn. */
-    
+    /// The kind of gradient that will be drawn.
     open var type: GradientType = .linear {
         didSet {
             setNeedsDisplay()
